bfa7b27d6957201a379740f6788cd198b606c817
stord-engineering-account <77021775+stord-engineering-account@users.noreply.github.com>
2023-08-10 12:20:12 -0600

    chore(main): release 1.2.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Datadog.DataStreams.MixProject do
       app: :data_streams,
       name: "Data Streams Ex",
       description: "DataDog data streams library for Elixir",
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
